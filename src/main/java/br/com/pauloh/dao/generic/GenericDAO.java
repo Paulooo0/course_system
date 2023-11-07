8ac7091e6ceac5aeff2f46db5e28ff1d51798238
Paulooo0 <paulohenrique.n.s00@gmail.com>
2023-11-06 23:10:01 -0300

    ADD: update, delete and findall methods in GenericDAO UPDATE: update and delete receives entity instead id

--- src/main/java/br/com/pauloh/dao/generic/GenericDAO.java
+++ src/main/java/br/com/pauloh/dao/generic/GenericDAO.java
@@ -1,5 +1,7 @@
 package br.com.pauloh.dao.generic;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -45,15 +47,51 @@ public class GenericDAO<T> implements IGenericDAO<T> {
     }
 
     @Override
-    public T update(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    public T update(T entity) {
+        EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("ExemploJPA");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.merge(entity);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
+        return entity;
     }
 
     @Override
-    public void delete(Long id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    public void delete(T entity) {
+        EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("ExemploJPA");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        T mergedEntity = em.merge(entity);
+
+        em.remove(mergedEntity);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
     }
     
+    @Override
+    public List<T> findAll() {
+        EntityManagerFactory emf = 
+            Persistence.createEntityManagerFactory("ExemploJPA");
+        EntityManager em = emf.createEntityManager();
+
+        List<T> entities = em
+            .createQuery("FROM " + entityClass.getSimpleName(), entityClass)
+            .getResultList();
+        
+        em.close();
+        emf.close();
+
+        return entities;
+    }
 }
--- src/main/java/br/com/pauloh/dao/generic/IGenericDAO.java
+++ src/main/java/br/com/pauloh/dao/generic/IGenericDAO.java
@@ -1,11 +1,15 @@
 package br.com.pauloh.dao.generic;
 
+import java.util.List;
+
 public interface IGenericDAO<T> {
     T create(T entity);
 
     T read(Long id);
 
-    T update(Long id);
+    T update(T entity);
 
-    void delete(Long id);
+    void delete(T entity);
+
+    List<T> findAll();
 }
